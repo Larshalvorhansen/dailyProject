c3233641dce22450b6948e95ee0890616d926371
larshalvorhansen <larshalvorhansen1@gmail.com>
2025-11-22 22:35:06 +0100

    Add: day 2

---
+++ day2.py
@@ -0,0 +1,6 @@
+# Create a program that analyzes text files to find word frequency distributions and export results to CSV.
+# Include functionality to find the most common words and basic statistics.
+
+def textToList(text):
+    for i in 
+    return list
