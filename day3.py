6910c4254df689f423eac23714f028a0c4ace97d
larshalvorhansen <larshalvorhansen1@gmail.com>
2025-11-25 11:10:44 +0100

    Continue: day 3

--- day3.py
+++ day3.py
@@ -1,5 +1,16 @@
-# Build a simple budget tracker that categorizes expenses and generates monthly summaries. 
+# Build a simple budget tracker that categorizes expenses and generates monthly summaries.
 # Allow users to add transactions and view spending by category.
 
-def collectExpenses():
+import csv
 
+def collectExpenses(day):
+    thing = input("what thing have you bouth? ")
+    price = input("what price was it? ")
+    category = input("waht category would you like to place it in? ")
+    day = day 
+    with open('expenses.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.
+
+for days in range(1,31):
+    collectExpenses(day)
