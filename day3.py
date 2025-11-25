58fc81cf7d4f3cbcd699798e8c416590c8700170
larshalvorhansen <larshalvorhansen1@gmail.com>
2025-11-25 10:41:40 +0100

    Start day 3

---
+++ day3.py
@@ -0,0 +1,5 @@
+# Build a simple budget tracker that categorizes expenses and generates monthly summaries. 
+# Allow users to add transactions and view spending by category.
+
+def collectExpenses():
+
