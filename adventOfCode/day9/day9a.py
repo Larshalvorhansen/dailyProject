af64b28e34501600e9eeca5d5ccf2b247c61aa18
larshalvorhansen <larshalvorhansen1@gmail.com>
2025-12-10 01:21:35 +0100

    Finish: day9 task 1

---
+++ adventOfCode/day9/day9a.py
@@ -0,0 +1,37 @@
+def getData():
+    data = []
+    with open("input", "r") as file:
+        for line in file:
+            line = line.strip()
+            line = list(map(int, line.split(",")))
+            data.append(line)
+
+    return data
+
+
+def getSquareSize(p1, p2):
+    size = (abs(p1[0] - p2[0]) + 1) * (abs(p1[1] - p2[1]) + 1)
+    temp = []
+    temp.append(p1)
+    temp.append(p2)
+    return size
+
+
+def main():
+    sum = 0
+    data = getData()
+    print(data)
+
+    temp = [getSquareSize(i, j) for i in data for j in data]
+    print(temp)
+
+    print(max(temp))
+
+
+if __name__ == "__main__":
+    main()
+    print("Done")
+
+"""
+Sudocode
+"""
---
+++ adventOfCode/day9/template.py
@@ -0,0 +1,25 @@
+def getData():
+    data = []
+    with open("input", "r") as file:
+        for line in file:
+            line = line.strip()
+            data.append(line)
+
+    return data
+
+
+def main():
+    sum = 0
+    data = getData()
+
+    [print(f"Data: \n{i}") for i in data]
+    print(sum)
+
+
+if __name__ == "__main__":
+    main()
+    print("Done")
+
+"""
+Sudocode
+"""
